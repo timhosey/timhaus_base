795a78603cadb34724ba3635811a9b5f30faa17c
Tim Hosey <insertcointheater@gmail.com>
2020-09-27 18:13:57 -0400

    corrected spacing to prevent further failure

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,6 +1,6 @@
 # Tests right here
 if os.family == 'debian'
-    describe packages('nfs-common') do
-        its('statuses') { should cmp 'installed' }
-    end
+  describe packages('nfs-common') do
+    its('statuses') { should cmp 'installed' }
+  end
 end
