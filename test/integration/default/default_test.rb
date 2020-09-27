287d7907aac61700d9cd0a8ecb9c90245f5285d6
Tim Hosey <insertcointheater@gmail.com>
2020-09-27 18:30:39 -0400

    testing

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -3,4 +3,13 @@ if os.family == 'debian'
   describe packages('nfs-common') do
     its('statuses') { should cmp 'installed' }
   end
+  describe packages('vim') do
+    its('statuses') { should cmp 'installed' }
+  end
+  describe packages('figlet') do
+    its('statuses') { should cmp 'installed' }
+  end
+  describe packages('docker') do
+    its('statuses') { should cmp 'installed' }
+  end
 end
